15a2a63c6e1c12e19665f2eb46d87fee1b1b0b24
Luziberto Mendes <lzuibertomendesjunior@hotmail.com>
2023-05-21 15:31:29 -0300

    Improvement musics js

--- public/js/musics.js
+++ public/js/musics.js
@@ -1,15 +1,25 @@
-let season = Math.floor((Math.random() * 5) + 1)
-let music = new Audio(`../../public/audio/pokemon_op_${season}.mp3`)
+
+let music = null
+let currentSeason = 0
 
 pokeball.addEventListener('click', () => {
-  if (music.played) changeMusic()
-  music.play()
+  changeMusic(music, generateDifferentSeason(currentSeason))
 })
 
-const changeMusic = () => {
-  music.pause()
-  season = Math.floor((Math.random() * 5) + 1)
-  music = new Audio(`../../public/audio/pokemon_op_${season}.mp3`)
+const changeMusic = (currentMusic, season) => {
+  if (currentMusic) currentMusic.pause()
+  const newMusic = new Audio(`../../public/audio/pokemon_op_${season}.mp3`)
+  newMusic.play()
+  music = newMusic
 }
 
-pokeball.click()
+const generateDifferentSeason = (currentSeason) => {
+  const newSeason = Math.floor((Math.random() * 5) + 1)
+  if (currentSeason === newSeason) generateDifferentSeason(currentSeason)
+  else currentSeason = newSeason
+  return currentSeason
+}
+
+setInterval(() => {
+  if (!music || !music.currentTime || music.ended) changeMusic(music, generateDifferentSeason(currentSeason))
+}, 1000)
